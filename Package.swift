dfc7d36ac927f75b9e29dd2712456557e693b3f7
Aldrich Co <aldrich@anyline.com>
2023-07-19 17:38:41 +0200

    Update binary checksum

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         .binaryTarget(
             name: "AnylineTireTreadSdk",
             url: "https://storage.googleapis.com/anyline-ttr-ios-artifacts/AnylineTireTreadSdk_SPM_2.1.0-preview.1.zip",
-            checksum: "807cd71be7357b5c1ab4baa9412b02419dd77421077407b145f134703d7c865c"
+            checksum: "a7352f285525f159c6a78b03775bb88ef2ecc9467a07928d601853c24d067e8b"
         )
     ]
 )
