b805e64cc1cdc55c3d67d9450c457724741eee3d
Aldrich Co <aldrich@anyline.com>
2023-07-20 15:16:27 +0200

    Update binary to 2.1.0-spm

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AnylineTireTreadSdk",
-            url: "https://storage.googleapis.com/anyline-ttr-ios-artifacts/AnylineTireTreadSdk_2.1.0.zip",
+            url: "https://storage.googleapis.com/anyline-ttr-ios-artifacts/AnylineTireTreadSdk_spm_2.1.0.zip",
             checksum: "b2f1605e0c06bd31862f9673121a8ccf5637e105d390e11876c29fa287a018e8"
         )
     ]
