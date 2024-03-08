4102dcc7baf7fe03d6ab615a2d849e5b11e0f027
Your Name <you@example.com>
2024-03-08 09:29:40 +0000

    4.1.0

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AnylineTireTreadSdk",
-            url: "https://ttr-sdk-ios.anyline.io/AnylineTireTreadSdk_spm_4.0.1.zip",
-            checksum: "089800f29d151e315a34a6365264dfce2a564d13f558b6140a9921a7acdd25bb"
+            url: "https://ttr-sdk-ios.anyline.io/AnylineTireTreadSdk_spm_4.1.0.zip",
+            checksum: "75d2f80aaac46e8b923b60c0eac9f1c3fa4523d129925b1a800986d539b3da84"
         )
     ]
 )
