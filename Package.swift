5ce9f8aac4d8aa5ad72b8be2329c055ffc01a25c
Measure Team <measure-team@anyline.com>
2026-06-12 12:29:10 +0000

    15.1.0

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AnylineTireTreadSdk",
-            url: "https://ttr-sdk-ios.anyline.io/stable/15.0.0/AnylineTireTreadSdk_spm_15.0.0.zip",
-            checksum: "e16c7f331c20c866ec99b2a3762fc9c75ca3bd5ba1b5f8fdaa26fcc986c04383"
+            url: "https://ttr-sdk-ios.anyline.io/stable/15.1.0/AnylineTireTreadSdk_spm_15.1.0.zip",
+            checksum: "e35fe3d549daf9cbc821fcfe094eadf6491dad600c4fbc07e208ccdcf9836408"
         )
     ]
 )
